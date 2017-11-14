bd04039f8d2992d701539640c6a2f3b05fc7849a
Vadim Komisarchik <vadim@myclok.com>
2017-11-13 22:12:18 -0500

    added initial files

---
+++ LeakFinder/LeakFinder/LeakFinder.swift
@@ -0,0 +1,18 @@
+//
+//  LeakFinder.swift
+//  LeakFinder
+//
+//  Created by Vadim Komisarchik on 11/13/17.
+//  Copyright © 2017 Myclok. All rights reserved.
+//
+
+import Foundation
+
+struct LeakFinder {
+    static let sharedInstance = LeakFinder()
+    
+    func startTracking() {
+        
+    }
+}
+
